31918b21eb78f7c471bce8ed5e4f8951f5c38195
Shishir Sharma <260073@via.dk>
2018-12-18 13:22:22 +0100

    first commit after 1st and 2nd task

---
+++ TemperatureConverte/Converter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemperatureConverter
+{
+    public class Converter
+    {
+        public int temperatureToCelsius(int fahrenheit)
+        {
+            int celsius;
+            return celsius = (fahrenheit - 32) * 5 / 9;
+        }
+
+        public int temperatureToFahrenheit(int celsius)
+        {
+            int fahrenheit;
+            return fahrenheit = (celsius * 9 / 5) + 32;
+        }
+    }
+}
---
+++ TestTemperatureConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemperatureConverter;
+
+namespace TestTemperatureConverter
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Converter tc = new Converter();
+
+            Console.WriteLine("The temp is " + tc.temperatureToFahrenheit(20));
+
+            Console.WriteLine(tc.temperatureToCelsius(32));
+
+            Console.ReadKey();
+
+        }
+    }
+}
